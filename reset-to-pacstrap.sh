46f8696cb5b56a77d8aca636eb5089181872294e
0x3508a <124862084+0x3508A@users.noreply.github.com>
2023-08-25 12:31:05 +0530

    Adding basic scripts

---
+++ reset-to-pacstrap.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+## Stop the Display Manger
+# sudo systemctl disable gdm
+# sudo systemctl disable lightdm
+# sudo systemctl disable sddm
+
+## Mark Everything as Dependency
+# sudo pacman -D --asdeps $(pacman -Qqe)
+
+## Mark pacstrap packages as explicit
+
+# sudo pacman -D --asexplicit base linux linux-firmware \
+#	sof-firmware vim nano git man-db man-pages \
+#	bash-completion intel-ucode
+
+# sudo pacman -D --asexplicit base linux linux-firmware \
+#	sof-firmware vim nano git man-db man-pages \
+#	bash-completion amd-ucode
+
+## Take into Root user as otherwise it would not work
+# su -
+# Dangerous command
+# pacman -Qttdq | pacmans -Rns -
+
