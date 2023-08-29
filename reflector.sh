5145888bb92b122b529aff24a5c04bfee6cc25d5
0x3508a <124862084+0x3508A@users.noreply.github.com>
2023-08-29 23:13:01 +0530

    fix reflector issue

--- reflector.sh
+++ reflector.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 sudo reflector --country India,Singapore,Japan,"South Korea","United States" \
-	--protocol https --threads 4 --age 6 --latest 10 \
+	--protocol https --threads 4 --age 6 \
 	--sort rate --save /etc/pacman.d/mirrorlist
